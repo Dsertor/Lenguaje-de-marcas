2669e87e2fa81f510716e43c29310dbf446bdf91
Sergio <s.rolandomartinez@gmail.com>
2022-12-19 20:38:28 +0100

    last changes

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
-const backToTop = document.getElementById("backToTop");
+/*------------------------------*/
+/*--------INITIALIZATION--------*/
+/*------------------------------*/
 
-const filmImage = document.querySelectorAll(".filmImage");
+const backToTop = document.getElementById("backToTop");
 const menu = document.getElementById("menu");
-const maxWidthNav = window.matchMedia("(max-width: 768px)");
 const headerContainer = document.getElementById("headerContainer");
-const logoContainer = document.getElementById("logoContainer");
 const mainNav = document.getElementById("mainNav");
 const ulMainNav = document.getElementById("ulMainNav");
-const liMainNav = document.getElementsByClassName("navSide");
 const hamburguer = document.getElementById("hamburguer");
+const logoContainer = document.getElementById("logoContainer");
+
+const liMainNav = document.getElementsByClassName("navSide");
+const filmImage = document.querySelectorAll(".filmImage");
+const maxWidthNav = window.matchMedia("(max-width: 768px)");
+
 const liMainNavArray = Array.from(liMainNav);
 
 ToTheTop();
 
+/*------------------------------*/
 /*------------EVENTS------------*/
+/*------------------------------*/
 
+/* When the scroll bar of an element is moved, it calls the ToTheTop function*/
 window.addEventListener('scroll', ToTheTop);
+
+/* When the element is clicked, it brings the scroll bar to the top of the page */
 backToTop.addEventListener('click', () =>{
   window.scrollTo(0,0);
 }); 
 
-
+/* Create two events, one when hovering over an element and the other when leaving it, for each element in an array*/
 for(let element of filmImage){
   element.addEventListener("mouseenter", () =>{
     element.children[0].children[1].classList.add("filmCardOn");
@@ -34,6 +44,7 @@ for(let element of filmImage){
   });
 }
 
+/* Create a click event for an element, adding or removing classes on the menu icon*/ 
 menu.addEventListener("click",() =>{
   if(!hamburguer.classList.contains("openNav")){
     hamburguer.classList.add("openNav");
@@ -57,9 +68,11 @@ menu.addEventListener("click",() =>{
 
 });
 
-/*------------FUNCTIONS------------*/
+/*------------------------------*/
+/*----------FUNCTIONS-----------*/
+/*------------------------------*/
 
-
+/* Function that shows or hides the "go to top" icon at the top of the page*/
 function ToTheTop() {
   if (window.pageYOffset == 0) {
     backToTop.style.display = "none";
